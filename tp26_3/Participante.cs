5e1e325073d2113905c47b0e61b46cbadac96c56
Unlucky204 <Lautaroparondo@gmail.com>
2025-03-26 22:09:19 -0300

    diagnostico en proceso

---
+++ tp26_3/Participante.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp26_3
+{
+    public class Participante()
+    {
+        public int NumParti {  get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public double Tiempo { get; set; }
+        public int Altura { get; set; }
+        public int Categoria { get; set; }
+        public int Op = 1;
+        public int i = 0;
+
+        //public DateOnly FechaNac {  get; set; }
+        //public int Edad 
+        //{
+        //    get
+        //    {
+        //        return DateTime.Now.Year - FechaNac.Year;
+        //    } 
+        //}
+
+        public void carga(int numParti)
+        {
+            Console.WriteLine("ingrese el nombre");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("ingrese el apellido");
+            Apellido = Console.ReadLine();
+            Console.WriteLine("ingrese el tiempo");
+            Tiempo = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese la altura");
+            Altura = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese la categoria");
+            Categoria = int.Parse(Console.ReadLine());
+        }
+    }
+}
--- tp26_3/Program.cs
+++ tp26_3/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tp26_3;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+int Op = 1;
+List<Participante> Parti = new List<Participante>();
+List<int> categoria = new List<int>();
+while (Op != 0)
+{
+    Console.WriteLine("ingrese el num. de participante");
+    Console.WriteLine("ingrese 0 si quiere salir");
+    int NumParti  = int.Parse(Console.ReadLine());
+    if (NumParti != 0)
+    {
+
+        Participante participantes = new Participante();
+        participantes.carga(NumParti);
+        foreach (int cat in categoria)
+        {
+            if (participantes.Categoria != cat)
+            {
+                categoria.Add(participantes.Categoria);
+            }
+        }
+        Parti.Add(new Participante());
+    }
+    else
+    {
+        Op = 0;
+    }
+
+}
+Participante? participanteMenorTiempo;
+foreach ( Participante p in Parti)
+{
+    int tiempoAux = int.MaxValue;
+    if (p.Tiempo < tiempoAux)
+    {
+        participanteMenorTiempo = p;
+    }
+}
+
